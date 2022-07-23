b923d10bae1e6e100e454dc5e0178a453af5adc9
n0rdp0l <63914035+n0rdp0l@users.noreply.github.com>
2022-07-24 00:07:40 +0200

    name + link

--- IMDB/web_scraper.py
+++ IMDB/web_scraper.py
@@ -1,3 +1,4 @@
+from os import link
 from bitarray import test
 from bs4 import BeautifulSoup
 import requests
@@ -5,5 +6,8 @@ import requests
 html_text = requests.get('https://www.imdb.com/chart/top/?ref_=nv_mv_250').text
 soup = BeautifulSoup(html_text, 'lxml')
 movies = soup.find_all('td', class_ = 'titleColumn')
-link = movies.find('a')
-print(link)
+for movie in movies:
+    name = [a.text for a in movie.find_all('a', href = True)]
+    link = [a['href'] for a in movie.find_all('a', href = True)]
+    print(link)
+    print(name)
