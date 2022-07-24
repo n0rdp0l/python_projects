112fd8c55e65665fe64593f3e3584a2f5439556b
n0rdp0l <63914035+n0rdp0l@users.noreply.github.com>
2022-07-24 09:18:28 +0200

    working data frame

--- IMDB/web_scraper.py
+++ IMDB/web_scraper.py
@@ -2,12 +2,25 @@ from os import link
 from bitarray import test
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
 html_text = requests.get('https://www.imdb.com/chart/top/?ref_=nv_mv_250').text
 soup = BeautifulSoup(html_text, 'lxml')
 movies = soup.find_all('td', class_ = 'titleColumn')
+names = []
+links = []
 for movie in movies:
-    name = [a.text for a in movie.find_all('a', href = True)]
-    link = [a['href'] for a in movie.find_all('a', href = True)]
-    print(link)
-    print(name)
+    
+    name = movie.find('a', href = True).text
+    link =  movie.find('a', href = True)['href']
+
+    names.append(name)
+    links.append(link)
+
+    print(f'''
+    Movie Name: {name}
+    Link: {"www.imdb.com" + link}
+    ''')
+zipped = list(zip(names, links))
+df = pd.DataFrame(zipped, columns=['Name', 'Link'])
+print(df)
